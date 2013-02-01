10eb542ced9f4030bc3caa69cb6b4df45b04c5b2
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-02-01 04:22:50 +0000

    BUGFIX for ID double-up in checkboxField
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_newsletter/trunk@3918 950b781c-c544-efab-2f99-90083ee69683

--- code/extensions/NewsletterSignupDecoratorFormFixes.php
+++ code/extensions/NewsletterSignupDecoratorFormFixes.php
@@ -6,7 +6,8 @@ class NewsletterSignupDecoratorFormFixes extends Extension {
 	public function updateFields(&$fields) {
 		$fields->push(new HeaderField("NewsletterSignupHeader", _t("OrderForm.NEWSLETTER", "Newsletter"), 3));
 		$fields->push(new LiteralField("NewsletterSignupContent", "<p>"._t("OrderForm.SIGNUPTEASER", "Sign up to our newsletter to receive updates as they are released.")."</p>"));
-		$fields->push(new CheckboxField("NewsletterSignup", _t("OrderForm.SIGNUPTONEWSLETTER", "Sign up to our newsletter")));
+		$signUpField = new CheckboxField("NewsletterSignup", _t("OrderForm.SIGNUPTONEWSLETTER", "Sign up to our newsletter"), null, $this->owner);
+		$fields->push($signUpField);
 	}
 
 
